f91d6ae269907990e1cb446b30ffedc9ab39756d
sumeyye20 <71411837+sumeyye20@users.noreply.github.com>
2021-02-05 14:01:45 +0100

    mjdnjdnfmsdwnfdj msd0wd0dnsji9j0q

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("HALLO WORLD");
+        System.out.println("HALLO WORLD");
+        //anasbbbas
     }
 }
